aed2f5364c46db020afc593c39d87fa159c0665c
DMosqueral <dml2410@gmail.com>
2022-05-26 21:44:31 -0500

    "ADD --- Se resolvió la Part04Transform"

--- src/main/java/io/pivotal/literx/Part04Transform.java
+++ src/main/java/io/pivotal/literx/Part04Transform.java
@@ -13,27 +13,39 @@ public class Part04Transform {
 
 //========================================================================================
 
-	// TODO Capitalize the user username, firstname and lastname
-	Mono<User> capitalizeOne(Mono<User> mono) {
-		return null;
-	}
+    // TODO Capitalize the user username, firstname and lastname
+    Mono<User> capitalizeOne(Mono<User> mono) {
+        return mono.map(user -> new User(
+                user.getUsername().toUpperCase(),
+                user.getFirstname().toUpperCase(),
+                user.getLastname().toUpperCase()
+        ));
+    }
 
 //========================================================================================
 
-	// TODO Capitalize the users username, firstName and lastName
-	Flux<User> capitalizeMany(Flux<User> flux) {
-		return null;
-	}
+    // TODO Capitalize the users username, firstName and lastName
+    Flux<User> capitalizeMany(Flux<User> flux) {
+
+        return flux.map(user -> new User(
+                user.getUsername().toUpperCase(),
+                user.getFirstname().toUpperCase(),
+                user.getLastname().toUpperCase()
+        ));
+    }
 
 //========================================================================================
 
-	// TODO Capitalize the users username, firstName and lastName using #asyncCapitalizeUser
-	Flux<User> asyncCapitalizeMany(Flux<User> flux) {
-		return null;
-	}
-
-	Mono<User> asyncCapitalizeUser(User u) {
-		return Mono.just(new User(u.getUsername().toUpperCase(), u.getFirstname().toUpperCase(), u.getLastname().toUpperCase()));
-	}
+    // TODO Capitalize the users username, firstName and lastName using #asyncCapitalizeUser
+    Flux<User> asyncCapitalizeMany(Flux<User> flux) {
+//        return flux.flatMap(this::asyncCapitalizeUser);
+        return flux.flatMap(user -> asyncCapitalizeUser(
+                new User(user.getUsername(),
+                        user.getFirstname(),
+                        user.getLastname())));
+    }
+    Mono<User> asyncCapitalizeUser(User u) {
+        return Mono.just(new User(u.getUsername().toUpperCase(), u.getFirstname().toUpperCase(), u.getLastname().toUpperCase()));
+    }
 
 }
